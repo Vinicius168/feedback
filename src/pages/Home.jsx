f25b3093206cb36471b73d83130fa0a08a09e072
Vinicius 168 <viniciusgames168@gmail.com>
2024-06-10 12:34:27 +0000

    style: posicionamento do card no centro da tela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import {ButtonContainer, CardContainer} from "./Home.styles";
+
+import iconStarImg from "../assets/icon-star.svg"
+
 export function Home() {
   return (
-    <div>Home</div>
+    <CardContainer>
+      <iconStarImg>
+        <img src ={iconStarImg} alt="icone de estrela" />
+      </iconStarImg>
+
+
+      <h1>como foi o atendimento?</h1>
+      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimentol</p>
+
+      <ButtonContainer>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </ButtonContainer>
+      
+      <button>Enviar</button>
+    </CardContainer>
   )
 }
