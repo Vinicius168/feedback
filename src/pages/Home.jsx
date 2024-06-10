38a2e557162c7c7026af1d607d9cbc961cbace45
Vinicius 168 <viniciusgames168@gmail.com>
2024-06-10 13:49:16 +0000

    estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,45 @@
-import {ButtonContainer, CardContainer} from "./Home.styles";
+import {ButtonContainer, CardContainer, ResultContainer} from "./Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+
   return (
-    <CardContainer>
-      <iconStarImg>
-        <img src ={iconStarImg} alt="icone de estrela" />
-      </iconStarImg>
+    aparecerResultado === false ? (
+      <CardContainer>
+        <iconStarImg>
+          <img src ={iconStarImg} alt="icone de estrela" />
+        </iconStarImg>
 
 
-      <h1>como foi o atendimento?</h1>
-      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimentol</p>
+        <h1>como foi o atendimento?</h1>
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimentol</p>
 
-      <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-      </ButtonContainer>
-      
-      <button>Enviar</button>
-    </CardContainer>
+        <ButtonContainer>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </ButtonContainer>
+        
+        <button>Enviar</button>
+      </CardContainer>
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
+        
+        <ResultContainer>
+          <p>Voce selecionou 4 de 5</p>
+        </ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+
+      </CardContainer>
+    )
   )
 }
